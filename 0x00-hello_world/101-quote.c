ff9469d4b9137d688c0dfa695f6705c4c44e891a
fitsum mesfin <fitsum@root.dhcp02>
2022-03-10 16:46:48 +0530

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 /**
- * main - Entry point 
- *
- * Return: Always 1 (Success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
