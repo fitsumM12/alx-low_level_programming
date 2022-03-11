645755306a1b3c4686b6e0fa7719a85f7bee35a2
fitsum mesfin <fitsum@root.dhcp02>
2022-03-11 20:27:58 +0530

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,6 @@
 
 /*
  * main - Determines if a number is positive, negative or zero.
- *
  * Return: Always 0 (Success) 
  */
 int main(void)
@@ -13,17 +12,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if( n > 0 )
 	{
-		printf("%d is positive\n",n);
+	printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf(" %d is zero\n",n);
+	printf(" %d is zero\n", n);
 	}
 	else
 	{
-		printf(" %d is negative\n",n);
+	printf(" %d is negative\n", n);
 	}
 	return (0);
 }
