c12b47332425fb631da0e625e83e645f49cfe385
fitsum mesfin <fitsum@root.dhcp02>
2022-03-11 20:47:22 +0530

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - Determines if a number is positive, negative or zero.
- * Return: Always 0 (Success) 
- */
+* main - Determines if a number is positive, negative or zero.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	int n;
