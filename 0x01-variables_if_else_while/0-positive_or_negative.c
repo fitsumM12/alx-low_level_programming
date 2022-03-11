5fc069e72f765b3d10fc5968189dcde1d1706d50
fitsum mesfin <fitsum@root.dhcp02>
2022-03-11 20:06:54 +0530

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(n >0)
 	{
-	printf("%d is positive",n);
+	printf(" %d is positive",n);
 	}
 	else
 	{
-	printf("%d is negative",n);
+	printf(" %d is negative",n);
 	}
 	return (0);
 }
