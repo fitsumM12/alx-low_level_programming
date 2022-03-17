ef2d2a08ec6fdecc545d319522133a1888fa950a
fitsum mesfin <fitsum@root.dhcp02>
2022-03-17 23:45:02 +0530

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * print_remaining_days - takes a date and prints how many days are
  * left in the year, taking leap years into account
@@ -9,10 +8,9 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || ((year % 100 == 0) && year % 400 == 0)))
+	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 	{
 		if (month > 2 && day >= 60)
 		{
