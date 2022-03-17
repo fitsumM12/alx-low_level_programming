ee80937573acc72ca774244798cd56cb9e3812c5
fitsum mesfin <fitsum@root.dhcp02>
2022-03-17 08:25:53 +0530

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an integer
+ * @n: The integer to check
+ *
+ * Return: an integer which is the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
