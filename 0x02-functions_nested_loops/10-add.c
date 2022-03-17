9ea6b14adb8cab464980bb5245352e29c447af0a
fitsum mesfin <fitsum@root.dhcp02>
2022-03-17 08:34:38 +0530

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - Sums two integers and
+ * return the result.
+ *
+ * @num1: integer one.
+ * @num2: integer two.
+ *
+ * Return: Sum of the two integers
+ */
+
+int add(int num1, int num2)
+{
+
+	int sum = num1 + num2;
+
+	return (sum);
+}
