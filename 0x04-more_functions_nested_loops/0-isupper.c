2e720a179135a9107607fa62d3b1896bc70776bf
fitsum mesfin <fitsum@root.dhcp02>
2022-03-18 22:53:46 +0530

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - this function checks for uppercase character
+ * @c: The character to check
+ * Return: 1 if c is the uppercase , otherwise 0
+ */
+ 
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef main_h_
+#define main_h_
+
+#include <stdio.h>
+
+int _putchar(char c)
+int _isupper(int c)
+
+#endif
