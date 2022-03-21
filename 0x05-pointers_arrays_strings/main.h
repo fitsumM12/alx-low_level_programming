1b4a5a2ff8f6c23a5349532669ddadbcffe47401
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-21 13:01:00 -0700

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a int n);
+void print_array(int *a, int n);
 int *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
