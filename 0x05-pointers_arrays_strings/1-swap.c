92f975917ccc28f6883950612f16792f6b8ed52e
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-21 13:09:49 -0700

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,11 +7,10 @@
  */
 void swap_int(int *a, int *b)
 {
-  int first, second;
+int first, second;
 
-  first = *a;
-  second = *b;
-
-  *a = second;
-  *b = first;
+first = *a;
+second = *b;
+*a = second;
+*b = first;
 }
