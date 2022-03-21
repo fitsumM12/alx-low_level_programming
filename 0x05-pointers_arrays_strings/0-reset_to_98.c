0882b82c9870c27f7b7f24258717ea6576eee0d2
fitsumM12 <fitsummesfindejene@gmail.com>
2022-03-21 11:50:04 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_8 - check the code
+ * Return: always return 0.
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h_
+#define main_h_
+
+void reset_to_98(int *n);
+
+#endif
