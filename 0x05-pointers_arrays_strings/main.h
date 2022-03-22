4e08e06759abcfa675b8d25149481d6529f6b7dc
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 09:25:09 +0530

    update

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-int *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 
