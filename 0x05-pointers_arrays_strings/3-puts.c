738e6904cee464da5d6ddfc82d8687c0ca6aa35a
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 08:41:44 +0530

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _puts - This function prints a string, followed by a newline to the standard out
+ * _puts - This function prints a string, followed  
+ * newline to the standard out
  * @str: The string to print out to stdout
  */
 void _puts(char *str)
