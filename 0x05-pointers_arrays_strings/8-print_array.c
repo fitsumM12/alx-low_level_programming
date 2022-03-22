3fdb037e6b4c4e0dd75b5165fd3e14d21a73309b
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 09:13:23 +0530

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 
 /**
- * print_array - This function prints n elements of an array of integers
+ * print_array - This function prints n elements of
+ * an array of integers
  * @a: Array of integers to print
  * @n: Number of the array integers to print
  */
 void print_array(int *a, int n)
 {
-  int i;
+	int i;
 
-  for(i = 0; i < n; i++)
-    {
-      printf("%i", a[i]);
-      if(i != n-1)
-	printf(", ");
-    }
-
-  putchar('\n');
+	for(i = 0; i < n; i++)
+	{
+		printf("%i", a[i]);
+		if(i != n-1)
+			printf(", ");
+	}
+	_putchar('\n');
 }
