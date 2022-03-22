10d2ff7b07bb2e0ef4d209abecaacc3eb34fe923
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 08:45:07 +0530

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,15 @@
  */
 void print_rev(char *s)
 {
-  int i;
-  char ch;
+	int i;
+	char ch;
 
-  for  (i = 0; s[i] != 0; i++)
-    ;
-  for (i = i - 1; i >= 0; i--)
-    {
-      ch = s[i];
-      _putchar(ch);
-    }
-  _putchar('\n');
+	for  (i = 0; s[i] != 0; i++)
+		;
+	for (i = i - 1; i >= 0; i--)
+	{
+		ch = s[i];
+		_putchar(ch);
+	}
+	_putchar('\n');
 }
