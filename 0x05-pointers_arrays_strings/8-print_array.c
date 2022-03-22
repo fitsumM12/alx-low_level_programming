4c20f0c421f64ea23e7cc1f381f46d865f5b86fd
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 09:20:17 +0530

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%i", a[i]);
-		if(i != n-1)
+		if (i != n - 1)
 			printf(", ");
 	}
 	_putchar('\n');
