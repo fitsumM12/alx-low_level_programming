6ac77cd8041c9d4666cc8913eb6648a39b176040
fitsum mesfin <fitsum@root.dhcp02>
2022-03-22 09:03:22 +0530

    update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,15 @@
  */
 void puts_half(char *str)
 {
-  int i,j;
+	int i,j;
 
-  for (i = 0; str[i] != 0; i++)
-    ;
-  j = (i + 1) / 2;
-
-  for (; str[j] != 0; j++)
-    {
-      _putchar(str[j]);
-    }
-  _putchar('\n');
+	for (i = 0; str[i] != 0; i++)
+	{
+		j = (i + 1) / 2;
+		for (; str[j] != 0; j++)
+		{
+			_putchar(str[j]);
+		}
+		_putchar('\n');
+	}
 }
