2c57ac1dcc2661ad1ba258ff77a0f7ee1516be40
fitsum mesfin <fitsum@root.dhcp02>
2022-03-28 22:02:10 +0530

    chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _putchar(char);
 
 #endif
