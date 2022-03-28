08cdd427f98661a4a3f341a0cda5ee79e474dcba
fitsum mesfin <fitsum@root.dhcp02>
2022-03-28 22:08:21 +0530

    set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char);
-
+void print_diagsum(int *a, int size);
+void set_string(char **s, char *to);
 #endif
