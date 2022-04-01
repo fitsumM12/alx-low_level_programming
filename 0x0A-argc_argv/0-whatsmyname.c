9988f9ff3187383c67f6f7a8b236c8161022b0d4
fitsum mesfin <fitsum@root.dhcp02>
2022-04-01 15:33:16 +0530

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - prints the name of the file
+ * @argc: The number of the arguments
+ * @argv: The array of arguments
+ *
+ * Return: always return 0 Success
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,4 @@
+#ifndef main_h_
+#define main_h_
+
+#endif
