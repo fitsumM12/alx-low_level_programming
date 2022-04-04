1d80e8322a3095d6bd18698d1571ce332a7e8ac3
fitsum mesfin <fitsum@root.dhcp02>
2022-04-04 19:23:13 +0530

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,11 +16,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	ptr = malloc(sizeof(c) * size);
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
