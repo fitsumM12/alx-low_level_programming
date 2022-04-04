d922b13754c5c837a464f1df3513182e73a2296f
fitsum mesfin <fitsum@root.dhcp02>
2022-04-04 19:44:28 +0530

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: String
+ * Return: the pointer of an array of chars
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i, j;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	
+	ptr = (char *)malloc(sizeof(char) * (i + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j <= i; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,4 +2,6 @@
 #define main_h_
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
 #endif
