866642c06456c8a6513a9697aa9d9b29f59bea02
fitsum mesfin <fitsum@root.dhcp02>
2022-04-04 20:24:58 +0530

    free alloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,10 @@
 
 void free_grid(int **grid, int height)
 {
-	 if (grid != NULL && height != 0)
-	 {
-	 	for (; height >= 0; height--)
-	 		free(grid[height]);
-	 	free(grid);
-	 }
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
 }
