8377454f25c5144d2b7d91973c2c26ad076f213f
fitsum mesfin <fitsum@root.dhcp02>
2022-04-04 19:18:40 +0530

    update

---
+++ 0X0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - create the memory allocation for an array
+ * @size: size of the array.
+ * @c: the character
+ * Return: the pointer to the address or NULL if it fails.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(c) * size);
+	
+	if(ptr == NULL)
+	{
+		return (NULL);
+		
+	}
+	for(i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
---
+++ 0X0B-malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef main_h_
+#define main_h_
+
+char *create_array(unsigned int size, char c);
+#endif
