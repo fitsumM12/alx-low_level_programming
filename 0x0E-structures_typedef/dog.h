ae610c91c17ac92432cf78fdf0fd2b9fbe485fea
fitsum mesfin <fitsum@root.dhcp02>
2022-04-12 00:00:00 +0530

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _main_h_
+#define _main_h_
+
+/**
+ * dog - Typedef for struct dog
+ */
+typedef struct dog dog;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
