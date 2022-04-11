876ae379766bc5a9e9bf3baf26872910707ab29a
fitsum mesfin <fitsum@root.dhcp02>
2022-04-11 23:22:54 +0530

    update

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - prints the name of the file
+ * main - prints the name of the file.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
