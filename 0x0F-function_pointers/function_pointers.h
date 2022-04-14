937ccabdf25b908d83ccbc1cb7ed1b65c3d353ac
fitsum mesfin <fitsum@root.dhcp02>
2022-04-14 11:35:25 +0530

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef function_pointers_h
 #define function_pointers_h
 #include <stddef.h>
+
 void print_name(char *name, void (*)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
