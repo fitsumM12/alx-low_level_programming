3e3fa8bc31c872bacab3df1f5dfe8d0bb56693b4
fitsum mesfin <fitsum@root.dhcp02>
2022-04-14 11:09:32 +0530

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,2 +1,14 @@
-#include <stdio.h>
-#sq
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
