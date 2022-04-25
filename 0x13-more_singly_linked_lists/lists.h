e95f611861e8405fce59409ad1d84a78e059b4d4
fitsum mesfin <fitsum@root.dhcp02>
2022-04-25 09:56:30 +0530

    update

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+#endif
